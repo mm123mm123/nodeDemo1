d516a5ef102247acf1321a4f858d76be04038a2c
mm123mm123 <fiymi666@gmail.com>
2021-03-09 18:00:30 +0800

    加入换行但无效果

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const homedir = require('os').homedir();
 const home = process.env.HOME || homedir
 const dbPath = path.join(home, '.todo')
 module.exports.add = (taskName) => {
+    let taskList = []
     fs.readFile(dbPath, {flag: 'a+'}, (err, data) => {
-            const taskList = []
+            let newTask = {task: taskName.toString(), done: false}
             if (err) {
                 console.log(err);
-            } else if (data) {
-                console.log(data);
+            } else if (data.toString() === '') {
+                taskList.push(newTask)
+            } else {
+                taskList = JSON.parse(data.toString())
+                taskList.push(newTask)
             }
+            fs.writeFile(dbPath, JSON.stringify(taskList)+'\n', (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            })
+            console.log(data.toString());
         }
     )
+
 }
